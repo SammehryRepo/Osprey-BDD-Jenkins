b73e6dea78e8257ef2fa8ff63e45f0ff1adc382f
Shamsullah Mehry <shamsullah.mehry@tekschool.us>
2024-04-04 20:40:14 -0400

    BDD Framework for Jenkins

---
+++ src/test/java/tek/bdd/runner/TestRunner.java
@@ -0,0 +1,19 @@
+package tek.bdd.runner;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:features",
+        glue = "tek.bdd.steps",
+        tags = "@Smoke",
+        dryRun = false,
+       plugin = {
+        "pretty",
+              "html:target/html_report/report.html",
+               "json:target/jsonReport/report.json"
+
+      }
+)
+public class TestRunner {
+}
